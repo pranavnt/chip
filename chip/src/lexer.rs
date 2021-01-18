e3bff4b43a50ab16d4395fd859a1b040377d8530
Pranav Teegavarapu <pranavnt@outlook.com>
2021-01-17 16:45:23 -0800

    that works nowgit add .

--- chip/src/lexer.rs
+++ chip/src/lexer.rs
@@ -1,9 +1,7 @@
-#[derive(Debug, Clone)]
 pub struct Token {
     token_type: String,
     value: String,
 }
-
 
 /*
 helloWorld := fn() {
@@ -13,75 +11,69 @@ helloWorld := fn() {
 helloWorld();
 */
 
-
-// parens/braces/backets
-let L_PARENS = "L_PARENS";
-let R_PARENS = "R_PARENS"; 
-let L_BRACE = "L_BRACE";
-let R_BRACE = "R_BRACE";
-let L_SBRACKET = "L_BRACKET";
-let R_BRACKET = "R_BRACKET"; 
+const L_PARENS: &str = "L_PARENS";
+const R_PARENS: &str = "R_PARENS";
+const L_BRACE: &str = "L_BRACE";
+const R_BRACE: &str = "R_BRACE";
+const L_SBRACKET: &str = "L_BRACKET";
+const R_BRACKET: &str = "R_BRACKET";
 
 // comparison operators
-let EQUAL_OPERATOR = "EQUAL_OPERATOR";
-let NOT_EQUAL_OPERATOR = "NOT_EQUAL_OPERATOR";
-let GREATER_THAN_OPERATOR = "GREATER_THAN_OPERATOR";
-let LESS_THAN_OPERATOR = "LESS_THAN_OPERATOR";
-let GREATER_THAN_EQUAL_OPERATOR = "GREATER_THAN_EQUAL_OPERATOR";
-let LESS_THAN_EQUAL_OPERATOR = "LESS_THAN_EQUAL_OPERATOR";
+const EQUAL_OPERATOR: &str = "EQUAL_OPERATOR";
+const NOT_EQUAL_OPERATOR: &str = "NOT_EQUAL_OPERATOR";
+const GREATER_THAN_OPERATOR: &str = "GREATER_THAN_OPERATOR";
+const LESS_THAN_OPERATOR: &str = "LESS_THAN_OPERATOR";
+const GREATER_THAN_EQUAL_OPERATOR: &str = "GREATER_THAN_EQUAL_OPERATOR";
+const LESS_THAN_EQUAL_OPERATOR: &str = "LESS_THAN_EQUAL_OPERATOR";
 
 // logical operators
-let AND_OPERATOR = "AND_OPERATOR";
-let OR_OPERATOR = "OR_OPERATOR";
-let NOT_OPERATOR = "NOT_OPERATOR";
+const AND_OPERATOR: &str = "AND_OPERATOR";
+const OR_OPERATOR: &str = "OR_OPERATOR";
+const NOT_OPERATOR: &str = "NOT_OPERATOR";
 
 // assignment operators
-let INITIALIZE_OPERATOR = "INITIALIZE_OPERATOR";
-let ASSIGNMENT_OPERATOR = "ASSIGNMENT_OPERATOR";
-let PLUS_EQUALS = "PLUS_EQUALS";
-let MINUS_EQUALS = "MINUS_EQUALS";
+const INITIALIZE_OPERATOR: &str = "INITIALIZE_OPERATOR";
+const ASSIGNMENT_OPERATOR: &str = "ASSIGNMENT_OPERATOR";
+const PLUS_EQUALS: &str = "PLUS_EQUALS";
+const MINUS_EQUALS: &str = "MINUS_EQUALS";
 
 // math operators
-let ADDITION_OPERATOR = "ADDITION_OPERATOR";
-let SUBTRACTION_OPERATOR = "SUBTRACTION_OPERATOR";
-let MULTIPLICATION_OPERATOR = "MULTIPLICATION_OPERATOR";
-let DIVISION_OPERATOR = "DIVISION_OPERATOR";
-let MODULUS_OPERATOR = "MODULUS_OPERATOR";
+const ADDITION_OPERATOR: &str = "ADDITION_OPERATOR";
+const SUBTRACTION_OPERATOR: &str = "SUBTRACTION_OPERATOR";
+const MULTIPLICATION_OPERATOR: &str = "MULTIPLICATION_OPERATOR";
+const DIVISION_OPERATOR: &str = "DIVISION_OPERATOR";
+const MODULUS_OPERATOR: &str = "MODULUS_OPERATOR";
 
-pub enum Tokens {
-    L_PARENS,
-    R_PARENS,
-    R_BRACE,
-    L_BRACE,
+// conditionals
+const IF: &str = "IF";
+const ELSE: &str = "ELSE";
 
-    STRING,
-    INT,
+// loops
+const FOR_LOOP: &str = "FOR_LOOP";
+const WHILE_LOOP: &str = "WHILE_LOOP";
 
-    FUNCTION,
+//
+const FUNCTION: &str = "FUNCTION";
+const RETURN: &str = "RETURN";
 
-    INITIALIZE,
-    ASSIGN,
-
-    PRINTLN,
+#[derive(Debug, Clone)]
+pub struct Lexer {
+    text: String,
+    pos: i32,
+    current_char: String,
 }
 
-struct lexer {
-    text,
-    pos,
-    current_char,
-}
-
-impl Lexer for lexer {
-    
+impl Lexer {
+    fn print() {
+        println!("Hi");
+    }
 }
 
 pub fn lex(file: String) {
-
-    for current_char in file.chars()  {
+    for current_char in file.chars() {
         if current_char.is_numeric() {
-            println!("{}",current_char);
+            println!("{}", current_char);
         }
-        print!("{}",current_char);
-        
+        print!("{}", current_char);
     }
 }
